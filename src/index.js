569ad187e92c5d0aeb2826ee1c1c778841c74d80
Luis Pozo <lpozo94@gmail.com>
2021-12-13 15:44:29 -0500

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles/main.css'//importing to globally add tailwindcss
-
-ReactDOM.render(<App />, document.getElementById('root'));
